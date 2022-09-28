1ab030ccadbda9da01b4a0fec9384f7072d34b91
Pankaj <pankajx404@gmail.com>
2022-09-28 17:21:28 +0530

    adding .env.local file and firebase cofig data replace in evn file

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,23 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSGwhuzfoeY4NOqyUovDKFduHMTw0AIws",
-  authDomain: "contact-form-c6c94.firebaseapp.com",
-  databaseURL: "https://contact-form-c6c94-default-rtdb.firebaseio.com",
-  projectId: "contact-form-c6c94",
-  storageBucket: "contact-form-c6c94.appspot.com",
-  messagingSenderId: "840474384050",
-  appId: "1:840474384050:web:dfb3342d2f2a116cf19bab",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBSGwhuzfoeY4NOqyUovDKFduHMTw0AIws",
+//   authDomain: "contact-form-c6c94.firebaseapp.com",
+//   databaseURL: "https://contact-form-c6c94-default-rtdb.firebaseio.com",
+//   projectId: "contact-form-c6c94",
+//   storageBucket: "contact-form-c6c94.appspot.com",
+//   messagingSenderId: "840474384050",
+//   appId: "1:840474384050:web:dfb3342d2f2a116cf19bab",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
